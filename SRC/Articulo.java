995ff8f91c8e11d5bf0237f131f1ab969573e67a
Aythami <aythamimayorsanchez@gmail.com>
2026-02-12 20:38:37 +0100

    Creamos la clase Articulo con sus atributos

--- SRC/Articulo.java
+++ SRC/Articulo.java
@@ -1,1 +1,23 @@
+package SRC;
+public class Articulo {
+
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Articulo(String nombre, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public double getSubtotal() {
+        return cantidad * precio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + cantidad + " uds) - " + getSubtotal() + " €";
+    }
+}
 
