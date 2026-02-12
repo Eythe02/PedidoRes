010e383a0bc5485d4b38da28258f03caf84420ce
Aythami <aythamimayorsanchez@gmail.com>
2026-02-12 20:45:57 +0100

    Cream la clase Main la cual tendrá los objetos creados

--- SRC/Main.java
+++ SRC/Main.java
@@ -1,1 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pedido p = new Pedido("Lucía Gómez");
+
+        p.agregarArticulo(new Articulo("Pizza", 1, 8.50));
+        p.agregarArticulo(new Articulo("Agua", 2, 1.20));
+        p.agregarArticulo(new Articulo("Helado", 1, 3.00));
+
+        p.mostrar();
+
+        p.cambiarEstado(EstadoPedido.LISTO);
+        p.mostrar();
+
+        p.cambiarEstado(EstadoPedido.ENTREGADO);
+        p.mostrar();
+    }
+}
 
